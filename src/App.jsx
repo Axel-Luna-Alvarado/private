d4be73af60c9d3fab869142bf095823ab4945dc3
AlexAviles593 <alexanderavilesparrales@gmail.com>
2024-07-12 17:27:10 -0500

    Vista Principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Inicio from './pages/Inicio'
 import {Login} from './pages/Login'
 import { SignUp } from './pages/SignUp'
+import './styles/Inicio.css'
 
 function App() {
 
   return (
     <>
-     <SignUp/>
+     <Inicio/>
     </>
     
       
---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../assets/img/logo.png'
+
+const Inicio = () => {
+  return (
+    <div className="reservas-container d-flex justify-content-center align-items-center">
+      <div className="text-center text-white">
+        <div className="reservas-logo-container mb-3">
+        <img src={logo} alt="logo-img" className="reservas-logo" />
+        </div>
+        <h3 className="reservas-subtitulos">DESCUBRE NUESTRAS RESERVAS</h3>
+        <h1 className="reservas-titulo">Bienvenido a Reservas</h1>
+        <h3 className="reservas-subtulos text-center">GESTIÓN EFICIENTE DE RESERVAS</h3>
+        <button className="btn btn-light mt-7 reservas-button">Reservar</button>
+      </div>
+    </div>
+  );
+};
+
+export default Inicio;
